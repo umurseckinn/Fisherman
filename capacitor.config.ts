27735229e53113af87b16a5781a427a85736eba8
umurseckinn <umurseckinn@gmail.com>
2026-03-07 03:59:48 +0300

    updated cap

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.umurseckin.fishermansjourney',
-  appName: 'Fishermans Journey',
-  webDir: 'dist/public'
-};
-
-export default config;
